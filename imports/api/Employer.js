0b20da2d155fb6258ea07cb279651acaf573add9
yohance-lorrington <tabahanih@gmail.com>
2017-07-11 16:19:59 -0400

    Update Employer.js

--- imports/api/Employer.js
+++ imports/api/Employer.js
@@ -15,19 +15,23 @@ var schema = {
 
 Employer = new Mongo.Collection('Employers')
 
+Meteor.publish('employer-data', function publishfunct(userid){
+  this.ready();
+  return Employee.find({profileId: userId});
+});
+
 Meteor.methods({
   createEmployer(employer){
     if(!this.userId) throw new Meteor.Error('401',"Login required")
     check(employer,_.omit(schema,'_id'));
+    if( !(Employer.findOne({profileId: employer.profileId}) == null) ){
+      throw new Meteor.Error('403','Account already exists');
+    }
     Employer.insert(employer, function(err,ress){
       if(err){
         throw new Meteor.Error('BADBAD', 'Could not insert Employee');
       }else if ( ress) {
-        Meteor.publish('employer-data', function publishfunct(userid){
-          console.log(ress);
-          this.ready();
-          return Employee.find({profileId: userId});
-        });
+
 
       }
     });
