3c3a1f2c050dc6d9a40031b7097ee93fa8ef1c54
bilalillama <bilalahmed95@hotmail.com>
2017-07-23 23:39:25 -0400

    Employee user info component

---
+++ imports/client/Components/Dashboard/Profile/Components/EmpUserInfo.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Avatar from 'material-ui/Avatar';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+export default class EmpUserInfo extends React.Component{
+    constructor(props){
+        super(props);
+        var full_name = props['full_name'];
+        var job_positon =props['positon_name'];
+        var avatar_pic_1 = props['avatar_pic_1'];
+    }
+    render(){
+        return(
+            <MuiThemeProvider>
+                <Card zDepth={2} style={{width:'100%'}}>
+                    <CardMedia>
+                        <div id="UserInfoC">
+                            <div id="UserInfoLeft">
+                                <Avatar src={this.props.avatar_pic_1} size={180}/>
+                            </div>
+                            <div id="UserInfoRight">
+                                <h1>{this.props.full_name}</h1>
+                                <h2>{this.props.positon_name}</h2>
+                            </div>
+                        </div>
+                    </CardMedia>
+                </Card>
+            </MuiThemeProvider>
+        )
+    }
+}
