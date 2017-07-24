da9acf1acc3b17221dea4d461838ead3f8202bef
yohance-lorrington <tabahanih@gmail.com>
2017-07-24 12:49:09 -0400

    Update StepOne.js

--- imports/client/Components/RegisterLogin/StepOne.js
+++ imports/client/Components/RegisterLogin/StepOne.js
@@ -266,14 +266,13 @@ export default class StepOne extends Component {
                   type="password"
                 />
               }
-              {
-                this.state.showPassField &&
+
               <IconButton tooltip={
                 <div style={{textAlign:'left',whiteSpace:'normal',width:'100px'}}>
                   Password must contain at least 8 characters, a capital letter  a number and a special character ('!@#$%^&*')
                 </div>
               }>
-              }
+              
               
                 <ActionInfo color={grey500}/>
               </IconButton>
