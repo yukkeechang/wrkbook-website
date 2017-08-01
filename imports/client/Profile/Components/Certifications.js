91efb9a685a83d03c40c547b8b2d3486367a9554
Ted Shin <tdyshin@gmail.com>
2017-08-01 14:00:19 -0400

    Add certifications tab content

--- imports/client/Profile/Components/Certifications.js
+++ imports/client/Profile/Components/Certifications.js
@@ -10,7 +10,29 @@ export default class Certifications extends React.Component {
 
   render() {
     return (
-      <p>Certifications Section</p>
+      <div className="row">
+        <div className="col s12 m6">
+          <div className="card-panel">
+            <h4>Certifications</h4>
+            <div className="row">
+              <div className="col s6">
+                <p>Background Check</p>
+              </div>
+              <div className="col s6">
+                <p>Apex Painting School</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col s6">
+                <p>Driver's License</p>
+              </div>
+              <div className="col s6">
+                <p>OSHA 30</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
