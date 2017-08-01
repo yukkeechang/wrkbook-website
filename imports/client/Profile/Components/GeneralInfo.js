d2b3ef07d584cfc46d360bb2cb644383aa2326da
Ted Shin <tdyshin@gmail.com>
2017-08-01 13:15:53 -0400

    Change materialize tabs to buttons

--- imports/client/Profile/Components/GeneralInfo.js
+++ imports/client/Profile/Components/GeneralInfo.js
@@ -1,28 +1,22 @@
 import React from 'react';
 
-export default class ConProfile extends React.Component{
+export default class ConProfile extends React.Component {
   render() {
     return (
       <div className="row">
         <div className="col s12 m6">
-          <div className="card">
-            <div className="card-content">
-              <h4>John Grego</h4>
-              <div className="row">
-                <div className="col s8">
-                  <p>4.0</p>
-                </div>
-                <div className="col s4">
-                  <p>View all</p>
-                </div>
+          <div className="card-panel">
+            <h4>John Grego</h4>
+            <div className="row">
+              <div className="col s8">
+                <p>4.0</p>
               </div>
-              <h5>Painter</h5>
-              <p>Brooklyn, New York</p>
+              <div className="col s4">
+                <a href="#">View all</a>
+              </div>
             </div>
-            <div className="card-action">
-              <a href="#">This is a link</a>
-              <a href="#">This is a link</a>
-            </div>
+            <h5>Painter</h5>
+            <p>Brooklyn, New York</p>
           </div>
         </div>
       </div>
--- imports/client/Profile/ContractorProfile.js
+++ imports/client/Profile/ContractorProfile.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import GeneralInfo from './Components/GeneralInfo';
 
-export default class ConProfile extends React.Component{
+export default class ConProfile extends React.Component {
   render() {
     return (
       <div class="container">
@@ -11,13 +11,23 @@ export default class ConProfile extends React.Component{
             <GeneralInfo />
           </div>
           <div className="col s8">
-            <ul className="tabs">
-              <li className="tab col s3"><a className="active" href="#">About</a></li>
-              <li className="tab col s3"><a href="#">Contact</a></li>
-              <li className="tab col s3"><a href="#">Certifications</a></li>
-              <li className="tab col s3"><a href="#">Payment</a></li>
-              <li className="tab col s3"><a href="#">Reviews</a></li>
-            </ul>
+            <div className="row">
+              <div className="col">
+                <a className="waves-effect waves-light btn">About</a>
+              </div>
+              <div className="col">
+                <a className="waves-effect waves-light btn">Contact</a>
+              </div>
+              <div className="col">
+                <a className="waves-effect waves-light btn">Certifications</a>
+              </div>
+              <div className="col">
+                <a className="waves-effect waves-light btn">Payment</a>
+              </div>
+              <div className="col">
+                <a className="waves-effect waves-light btn">Reviews</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
