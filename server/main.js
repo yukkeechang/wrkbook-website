bd68221544cd84eb85e2192d5d735faa646bb4c1
yohance-lorrington <tabahanih@gmail.com>
2017-12-08 23:28:11 -0500

    Update main.js

--- server/main.js
+++ server/main.js
@@ -4,7 +4,7 @@ import './startup';
 import '../imports/api/Users';
 import '../imports/api/Jobs';
 import '../imports/api/References';
-// import '../imports/api/Images';
+import '../imports/api/Images';
 import '../imports/api/Notifications';
 import '../imports/api/Events';
 import '../imports/api/Reviews';
