17b4ee3a5348709dc57306ee8015dee55382577e
bilal ahmed <bilalahmed95@hotmail.com>
2018-01-20 11:34:15 -0500

    buttons on FAQ page now link to register

--- imports/client/Pages/FaqPage.js
+++ imports/client/Pages/FaqPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from './Shared/Header';
 import Footer from './Shared/Footer';
 export default class FaqPage  extends React.Component {
@@ -55,11 +56,15 @@ export default class FaqPage  extends React.Component {
                         <h6 className="flow-text">E-mail us at <a style={{color:'#26a69a'}}href="mailto:info@wrkbook.com?subject=[Information Inquiry]">info@wrkbook.com</a> or call us at 914-719-6422</h6>
                         <h4 className="center-align flow-text">Ready to work?</h4>
                         <div className="center-align">
-                          <a className="waves-effect waves-teal btn">Sign Up</a>
+                          <Link to="/register">
+                            <a className="waves-effect waves-light btn">Sign Up</a>
+                          </Link>
                         </div>
                         <h4 className="center-align flow-text">Ready to hire?</h4>
                         <div className="center-align">
-                          <a className="waves-effect waves-teal btn">Sign Up</a>
+                          <Link to="/register">
+                            <a className="waves-effect waves-light btn">Sign Up</a>
+                          </Link>
                         </div>
                       </div>
                     </div>
