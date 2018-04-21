414b86f580ff86d55f2024b8005044836d4e11d8
Tabahani <tabahanih@gmail.com>
2018-04-21 15:30:09 -0400

    added some test for notification API

--- imports/api/Notification.test.js
+++ imports/api/Notification.test.js
@@ -1,10 +1,45 @@
-import { Meteor } from 'meteor/meteor';;
+import { Meteor } from 'meteor/meteor';
 import { resetDatabase } from 'meteor/xolvio:cleaner';
 import { expect, be } from 'meteor/practicalmeteor:chai';
+import { Random } from 'meteor/random';
+import './Notifications';
 
-describe('dummy test',function(){
-  it('something that should be tested',function(){
-    let things = 3;
-    expect(things).to.equal(3);
-  });
+describe('Notification  API',function(){
+    const userId = Random.id();
+
+
+    before(function() {
+     resetDatabase();
+    });
+
+    it('can create a new Notification',function(){
+      let newNotify ={
+        toWhomst:userId,
+        seen:false,
+        description:'dummy text',
+        jobId:'someJob',
+        typeNotifi:'MATCH',
+        href:'/job/:id',
+        createdAt:new Date()
+      }
+      const newNotification = Meteor.server.method_handlers['createNotification'];
+      const invocation ={ userId};
+
+      newNotification.apply(invocation,[newNotify]);
+
+      expect(Notification.find().count()).to.equal(1);
+    });
+
+    it('can edit a Notification',function(){
+      const seenNotification = Meteor.server.method_handlers['updateNotification'];
+      const invocation = {userId};
+
+      let notify = Notification.findOne();
+      let notifyId = notify._id;
+      console.log(notify);
+      seenNotification.apply(invocation,[notifyId]);
+      expect(Notification.findOne().seen).to.equal(true);
+
+
+    });
 });
